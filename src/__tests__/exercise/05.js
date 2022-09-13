48f1b4a9035081acc311f96f25219d9a280969aa
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-09-12 22:55:58 -0600

    5,6,7,8

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -3,11 +3,14 @@
 
 import * as React from 'react'
 // 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
+import { rest } from 'msw';
+import { setupServer } from 'msw/node'
 import Login from '../../components/login-submission'
+import { handlers } from '../../test/server-handlers';
 
 const buildLoginForm = build({
   fields: {
@@ -15,6 +18,8 @@ const buildLoginForm = build({
     password: fake(f => f.internet.password()),
   },
 })
+
+const server = setupServer(...handlers)
 
 // 🐨 get the server setup with an async function to handle the login POST request:
 // 💰 here's something to get you started
@@ -28,6 +33,9 @@ const buildLoginForm = build({
 // 🐨 before all the tests, start the server with `server.listen()`
 // 🐨 after all the tests, stop the server with `server.close()`
 
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
   const {username, password} = buildLoginForm()
@@ -35,14 +43,49 @@ test(`logging in displays the user's username`, async () => {
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 uncomment this and you'll start making the request!
-  // await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   // as soon as the user hits submit, we render a spinner to the screen. That
   // spinner has an aria-label of "loading" for accessibility purposes, so
   // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
   // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
 
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
   // once the login is successful, then the loading spinner disappears and
   // we render the username.
   // 🐨 assert that the username is on the screen
+  expect(screen.getByText(username)).toBeInTheDocument();
+})
+
+test('omitting the password results in an error', async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  // don't type in the password
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(`"password required"`);
+})
+
+test('Unknown server error renders the error', async () => {
+  const testErrorMessage = 'Something went wrong'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    )
+  )
+
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,8 +2,8 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {renderHook, act} from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
 // 🐨 create a simple function component that uses the useCounter hook
@@ -13,10 +13,73 @@ import useCounter from '../../components/use-counter'
 // const {count, increment, decrement} = useCounter()
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  // const result = {};
+  //
+  // function TestComponent(props) {
+  //   Object.assign(result, useCounter(props));
+  //   return null;
+  // }
+
+
+  const { result } = renderHook(useCounter);
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
+
+test('allows customization of the initial count', () => {
+  // const result = {};
+  //
+  // function TestComponent(props) {
+  //   Object.assign(result, useCounter(props));
+  //   return null;
+  // 
+
+  const initialCount = 2;
+
+  const { result } = renderHook(useCounter, { initialProps: { initialCount }})
+  expect(result.current.count).toBe(initialCount)
+})
+
+
+test('allows customization of the step', () => {
+  // const result = {};
+  //
+  // function TestComponent(props) {
+  //   Object.assign(result, useCounter(props));
+  //   return null;
+  // }
+
+  const step = 3;
+
+  const { result } = renderHook(useCounter, {initialProps: { step } })
+  expect(result.current.count).toBe(0);
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+// testing without using the result = {} technique
+// test('exposes the count and increment/decrement functions', async () => {
+//   // 🐨 render the component
+//   render(<TestComponent />);
+//   // 🐨 get the elements you need using screen
+//   const incBtn = screen.getByText(/increment/i)
+//   const decBtn = screen.getByText(/decrement/i)
+//   const count = screen.getByText(/current count/i)
+//
+//   // 🐨 assert on the initial state of the hook
+//   expect(count).toHaveTextContent('current count: 0');
+//
+//   await userEvent.click(incBtn);
+//   expect(count).toHaveTextContent('current count: 1');
+//
+//   await userEvent.click(decBtn);
+//   expect(count).toHaveTextContent('current count: 0');
+//   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+// })
 
 /* eslint no-unused-vars:0 */
